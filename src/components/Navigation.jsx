bde623fe7900d59df17795203622e6920795e044
Mbali Sithebe <2567890@students.wits.ac.za>
2026-04-14 15:22:51 +0200

    Update Navigation.jsx

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,7 @@
+//What will be our state, context and effect? 
+import React, { useState, useContext, useEffect } from "react"; 
+import { useNavigate, useLocation } from "react-router-dom";
+
+export default function Navigation (){
+    
+}
