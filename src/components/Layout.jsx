4b1b3e3d8fec170ecd188dbee2579f4da5676ac5
Mbali Sithebe <2567890@students.wits.ac.za>
2026-04-15 16:01:48 +0200

    footer

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,13 +1,18 @@
 import Navigation from "./Navigation";
+import Footer from "./Footer";
 
 export default function Layout({ children }) {
     return (
         <div className="layout">
             <Navigation />
 
-            <main className="page-content">
-                {children}
-            </main>
+            <div className="content-wrapper">
+                <main className="page-content">
+                    {children}
+                </main>
+
+                <Footer /> {/* footer */}
+            </div>
         </div>
     );
 }
