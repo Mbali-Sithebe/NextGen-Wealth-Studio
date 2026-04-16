722d720cf016436f001e7bd2134dd8911eee3cce
Mbali Sithebe <2567890@students.wits.ac.za>
2026-04-16 12:00:35 +0200

    images

--- src/pages/MoneySnapshot.jsx
+++ src/pages/MoneySnapshot.jsx
@@ -7,9 +7,27 @@ export default function MoneySnapshot() {
 
     return (
         <Layout>
-            <h1>  
-            Welcome back{user?.username ? `, ${user.username}` : ""} 
-            </h1>
+            <header>
+                <section className="display-userContent">
+                    <h1 className="displayName">  
+                        Welcome back{user?.username ? `, ${user.username}` : ""} 
+                    </h1>
+
+                    <div className="logout-profile">
+                        <button>Log Out</button>
+                        <img className="userProfile" src="" alt="" />
+                        <img className="notification" src="" alt="" />
+                    </div>
+                </section>
+            </header>
+
+            <main>
+                <section>
+                    <h1>Money Snapshot</h1>
+                    <div className="date"></div>
+                </section>
+            </main>
+           
         </Layout>
     );
 }
