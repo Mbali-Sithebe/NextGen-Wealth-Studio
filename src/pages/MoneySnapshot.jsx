e80edc486382fc32bcccb05374905dc2f13629a0
Mbali Sithebe <2567890@students.wits.ac.za>
2026-04-16 12:14:35 +0200

    images

--- src/pages/MoneySnapshot.jsx
+++ src/pages/MoneySnapshot.jsx
@@ -2,27 +2,31 @@ import React, { useContext } from "react";
 import Layout from "../components/layout";
 import AuthContext from "../context/AuthContext";
 
+//Images Import Section
+import userProfileImg from "../images/user.png";
+import notificationImg from "../images/notification icon.png";
+
 export default function MoneySnapshot() {
     const { user } = useContext(AuthContext);
 
     return (
         <Layout>
             <header>
-                <section className="display-userContent">
+                <section className="user-Content">
                     <h1 className="displayName">  
                         Welcome back{user?.username ? `, ${user.username}` : ""} 
                     </h1>
 
                     <div className="logout-profile">
                         <button>Log Out</button>
-                        <img className="userProfile" src="" alt="" />
-                        <img className="notification" src="" alt="" />
+                        <img className="userProfile" src={userProfileImg} alt="User Profile" />
+                        <img className="notification" src={notificationImg} alt="Notifications" />
                     </div>
                 </section>
             </header>
 
             <main>
-                <section>
+                <section className="page-header">
                     <h1>Money Snapshot</h1>
                     <div className="date"></div>
                 </section>
