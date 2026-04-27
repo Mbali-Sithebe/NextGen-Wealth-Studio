cb56fe7141adf61f0e7a5c7d12360fc6cde4d56d
Mbali Sithebe <193603689+Mbali-Sithebe@users.noreply.github.com>
2026-04-27 12:47:40 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
-import { HashRouter, Routes, Route, Navigate } from 'react-router-dom'  
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import { useContext } from 'react'
 import { AuthProvider } from './context/AuthContext';
 import AuthContext from './context/AuthContext';
 
+//1. Pages
 import Login from "./pages/Login";
 import MoneySnapshot from './pages/MoneySnapshot';
 import StrategyTracks from './pages/StrategyTracks';
 import SimulationLab from './pages/SimulationLab';
 import EducationHub from './pages/EducationHub';
 
+//2. CSS
 import './App.css'
 import './styles/Login.css'
 import './styles/Navigation.css'
@@ -17,16 +19,16 @@ import './styles/StrategyTracks.css'
 import './styles/SimulationLab.css'
 import './styles/EducationHub.css'
 
+
 export default function App(){
     return (
         <AuthProvider>
-            <HashRouter>  
+            <BrowserRouter>
                 <AppRoutes/>
-            </HashRouter>
+            </BrowserRouter>
         </AuthProvider>
     )
 }
-
 
 function AppRoutes(){
     const { authStatus } = useContext(AuthContext);
@@ -35,13 +37,19 @@ function AppRoutes(){
         <Routes>
             <Route path='/' element={<Navigate to="/login"/>}/>
             <Route path='/login' element={<Login/>}/>
+            {/* MoneySnapshot is protected - only authed users can access the page */}
             <Route path='/MoneySnapshot' element={authStatus === "authed" ? 
             <MoneySnapshot/> : <Navigate to="/login"/>}/>
+            {/* Redirect any unknown user to Login */}
             <Route path='*' element={<Navigate to="/login"/>}/>
+
+            {/*Strategy Tracks*/}
             <Route path='/StrategyTracks' element={authStatus === "authed" ? 
             <StrategyTracks/> : <Navigate to="/login"/>}/>
+            {/*Simulation Lab*/}
             <Route path='/SimulationLab' element={authStatus === "authed" ? 
             <SimulationLab/> : <Navigate to="/login"/>}/>
+            {/*Education Hub*/}
             <Route path='/EducationHub' element={authStatus === "authed" ? 
             <EducationHub/> : <Navigate to="/login"/>}/>
         </Routes>
